6c1575e8d85dbb59955d1f611e240fceccf027f6
Nicco Kunzmann <niccokunzmann@googlemail.com>
2013-09-06 02:11:33 +0200

    fehler behoben

--- main/automatisch erkunden.py
+++ main/automatisch erkunden.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys, os
 sys.path.append(os.path.abspath('..'))
-from mousemove.programme.erkunde_ressourcen import main
+from mousemove.programme.erkunde_ressourcen import erkunde_ressourcen
 
-main()
+erkunde_ressourcen()
