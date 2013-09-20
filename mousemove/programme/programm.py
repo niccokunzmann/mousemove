28db7a050e076b8e5cb73b5f3c2c1e5ba62a120a
Nicco Kunzmann <niccokunzmann@googlemail.com>
2013-09-20 22:42:22 +0200

    program now disconnects after end

--- mousemove/programme/programm.py
+++ mousemove/programme/programm.py
@@ -1,4 +1,4 @@
-from .. import schedule
+from .. import schedule, disconnect
 from ..navigation import beep
 from ..errorhandling import report_exc, error_handling
 from .. import config
@@ -10,30 +10,33 @@ andere_waren_nicht_dran_zeit = 0.5
 
 def programm(funktion):
     def f(*args, **kw):
-        schedule.schedule()
-        config.load()
-        iterator = funktion(*args, **kw)
-        if isinstance(iterator, collections.Iterable):
-            while 1:
-                try:
-                    for timeout in iterator:
-                        if not timeout: timeout = 0
-                        letztes_scheduling = now = time.time()
-                        ende = letztes_scheduling + timeout
-                        schedule.schedule()
-                        while now < ende:
-                            # busy waiting
-                            if letztes_scheduling + andere_waren_nicht_dran_zeit > now:
-                                sleep_time = (1 if ende - letztes_scheduling > 1 else ende - letztes_scheduling)
-                                time.sleep(sleep_time)
-                            letztes_scheduling = now
+        try:
+            schedule.schedule()
+            config.load()
+            iterator = funktion(*args, **kw)
+            if isinstance(iterator, collections.Iterable):
+                while 1:
+                    try:
+                        for timeout in iterator:
+                            if not timeout: timeout = 0
+                            letztes_scheduling = now = time.time()
+                            ende = letztes_scheduling + timeout
                             schedule.schedule()
-                            now = time.time()
-                        config.load()
-                except KeyboardInterrupt: pass
-                except:
-                    raise report_exc()
-                else: break
+                            while now < ende:
+                                # busy waiting
+                                if letztes_scheduling + andere_waren_nicht_dran_zeit > now:
+                                    sleep_time = (1 if ende - letztes_scheduling > 1 else ende - letztes_scheduling)
+                                    time.sleep(sleep_time)
+                                letztes_scheduling = now
+                                schedule.schedule()
+                                now = time.time()
+                            config.load()
+                    except KeyboardInterrupt: pass
+                    except:
+                        raise report_exc()
+                    else: break
+        finally:
+            disconnect()
     f.__name__ = funktion.__name__
     f.__doc__ = funktion.__doc__
     return f
