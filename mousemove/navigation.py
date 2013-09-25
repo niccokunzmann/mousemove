9d551001ec1b2a70cb0766a4cbe3a6db44ca4086
Nicco Kunzmann <niccokunzmann@googlemail.com>
2013-09-25 12:15:54 +0200

    fehler beim dorfauswählen behoben

--- mousemove/navigation.py
+++ mousemove/navigation.py
@@ -73,16 +73,16 @@ def öffne_dorf_auf_karte(name = ''):
     from .auslesen import dorfname
     assert name is not None
     dorfnamen = set()
-    while 1:
+    for i in range(20):
         click(*rechts(933, 61))
         t = time.time()
         _dorfname = None
         _dorfname = dorfname()
-        if name == _dorfname or not name: break
-        if _dorfname in dorfnamen:
-            raise ValueError('Dorf {} konnte nicht unter den Dörfern {} '\
-                             'gefunden werden.'.format(repr(name), dorfnamen))
         dorfnamen.add(_dorfname)
+        if name == _dorfname or not name: break
+    if name and name != _dorfname:
+        raise ValueError('Dorf {} konnte nicht unter den Dörfern {} '\
+                         'gefunden werden.'.format(repr(name), dorfnamen))
     s = 1.8 - time.time() + t
     if s > 0: time.sleep(s) # warten bis er zum dorf gescrollt hat
 
