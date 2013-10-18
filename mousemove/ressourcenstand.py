395dcd0559ff78966f955381ccc7bde95251929a
Nicco Kunzmann <niccokunzmann@googlemail.com>
2013-10-18 13:08:53 +0200

    ressourcen positionen refactored

--- mousemove/ressourcenstand.py
+++ mousemove/ressourcenstand.py
@@ -4,6 +4,7 @@ from .positionen import mitte, erneuere_position, bild_positionen
 from . import mouse
 from . import config
 from . import auslesen
+from .markt import *
 
 dorf_ist_offen = None
 
@@ -56,25 +57,31 @@ class Ressourcenstand(dict):
                 d[ware] = stand - config.waren_verkaufs_schwellwert[ware]
         return d
 
-RESSOURCEN = mitte(255, 250)
-NAHRUNG = mitte(329, 250)
-WAFFEN = mitte(412, 250)
-BANKETTWAREN = mitte(485, 248)
+_handelspositionen = None
+def handelspositionen():
+    global _handelspositionen
+    if _handelspositionen is not None:
+        return _handelspositionen
+    
+    RESSOURCEN = mitte(255, 250)
+    NAHRUNG = mitte(329, 250)
+    WAFFEN = mitte(412, 250)
+    BANKETTWAREN = mitte(485, 248)
 
-eintrag = [(245, 357), (250, 396), (252, 439), (257, 473), (254, 516), (257, 558), (259, 600), (261, 631)]
-handelspositionen = {}
-for MENUPOSITION, waren in [(RESSOURCEN, 'holz stein eisen pech'),
-                                 (NAHRUNG, 'äpfel käse fleisch brot gemüse fisch bier'),
-                                 (WAFFEN, 'bögen piken rüstung schwerter katapulte'),
-                                 (BANKETTWAREN, 'wild möbel metallwaren gewänder wein salz gewürze seide')
-                                 ]:
-    for i, ware in enumerate(waren.split()):
-        handelspositionen[ware.lower()] = (MENUPOSITION, eintrag[i], i)
-
-del RESSOURCEN, NAHRUNG, WAFFEN, BANKETTWAREN, eintrag, i, waren, ware
+    eintrag = [(245, 357), (250, 396), (252, 439), (257, 473), (254, 516), (257, 558), (259, 600), (261, 631)]
+    handelspositionen = {}
+    for MENUPOSITION, waren in [(RESSOURCEN, 'holz stein eisen pech'),
+                                     (NAHRUNG, 'äpfel käse fleisch brot gemüse fisch bier'),
+                                     (WAFFEN, 'bögen piken rüstung schwerter katapulte'),
+                                     (BANKETTWAREN, 'wild möbel metallwaren gewänder wein salz gewürze seide')
+                                     ]:
+        for i, ware in enumerate(waren.split()):
+            handelspositionen[ware.lower()] = (MENUPOSITION, eintrag[i], i)
+    _handelspositionen = handelspositionen
+    return handelspositionen
 
 def vorhandene_warenmenge(name):
-    mengen_position = handelspositionen[name][2]
+    mengen_position = handelspositionen()[name][2]
     
 
 def ziehe_verkaufsschieber_nach_links():
@@ -93,8 +100,9 @@ def es_gibt_noch_händler():
 
 def click_verkaufen():
     mouse.click(*mitte(1073, 542))
+    return richtige_börse()
 
-def falsche_börse():
+def richtige_börse():
     p1 = erneuere_position('handelsfehler links')
     p2 = erneuere_position('handelsfehler rechts')
     assert bool(p1) == bool(p2), 'Das Fenster muss vollständig sein'
@@ -104,6 +112,15 @@ def falsche_börse():
         return False
     return True
 
+def vorhandene_warenmenge_im_handel(ware):
+    index = handelspositionen()[ware][2]
+    warenmenge = auslesen.warenmenge_im_handel(index) # nur für delta, liest nicht richtig
+    return warenmenge
+
+def setze_auf_einen_händler():
+    ziehe_verkaufsschieber_nach_links()
+    verkauf_einen_händler_mehr()
+
 class WarenZuVerkaufen(dict):
     def __init__(self, dorfname, other = {}):
         self.dorfname = dorfname
@@ -111,40 +128,49 @@ class WarenZuVerkaufen(dict):
 
     @öffnen(öffne_handel)
     def öffne_ware(self, ware):
-        menuposition, warenposition = handelspositionen[ware.lower()][:2]
+        menuposition, warenposition = handelspositionen()[ware.lower()][:2]
         mouse.click(*menuposition)
         mouse.click(*warenposition)
 
     @öffnen(öffne_handel)
     def verkaufe_ware(self, ware):
-        """=> ob noch Händler vorhanden sind"""
+        """=> Anzahl der losgeschickten Händler"""
         if ware not in self:
-            return False
+            return 0
         self.öffne_ware(ware)
-        index = handelspositionen[ware][2]
-        warenmenge0 = auslesen.warenmenge_im_handel(index) # nur für delta, liest nicht richtig
-        print('noch', warenmenge0, 'ware vorhanden.')
-        ziehe_verkaufsschieber_nach_links()
-        verkauf_einen_händler_mehr()
+        setze_auf_einen_händler()
         if not es_gibt_noch_händler():
-            return False
-        click_verkaufen()
-        warenmenge1 = auslesen.warenmenge_im_handel(index) # nur für delta, liest nicht richtig
-        print('noch', warenmenge1, 'ware vorhanden.')
-        
+            return 0
+        for i in range(wechsele_markt()):
+            if click_verkaufen():
+                # börse stimmt
+                return 1
+            else:
+                # börse wechseln
+                falscher_markt()
+                wechsele_markt()
 
     @öffnen(öffne_handel)
     def verkaufe_alles(self):
+        händler = 0
         for ware in self:
-            if not self.verkaufe_ware(ware):
+            gesendete_händler = self.verkaufe_ware(ware)
+            if not gesendete_händler:
                 break
+            händler += gesendete_händler
+        return händler
 
     def __repr__(self):
         super_string = super().__repr__()
         return '{}({}, {})'.format(self.__class__.__name__,
                                    repr(self.dorfname), super_string)
 
+    def __iter__(self):
+        keys = list(super().keys())
+        keys.sort(key = lambda ware: self[ware])
+        return iter(keys)
+
 __all__ = ['Ressourcenstand', 'WarenZuVerkaufen', 'es_gibt_noch_händler', 
-           'ziehe_verkaufsschieber_nach_links']
+           'ziehe_verkaufsschieber_nach_links', 'richtige_börse']
 
 
