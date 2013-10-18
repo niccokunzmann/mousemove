5985ac8f23fe808c0b544e3191f8b3eb303af96d
Nicco Kunzmann <niccokunzmann@googlemail.com>
2013-10-18 13:24:18 +0200

    programme anhalten verbessert

--- mousemove/programme/programme_anhalten.py
+++ mousemove/programme/programme_anhalten.py
@@ -1,8 +1,15 @@
-from tkinter.messagebox import showinfo
+from tkinter import *
 from .programm import programm
 
 @programm
 def programme_anhalten():
-    showinfo("Weiter machen?", "Die Programme wurden angehalten.\n OK klicken zum weitermachen.")
+    t = Tk()
+    t.title("Weiter machen?")
+    l = Label(t, text = "Die Programme wurden angehalten.\n OK klicken zum weitermachen.")
+    l.pack()
+    b = Button(t, text = 'OK', command = t.quit)
+    b.pack()
+    t.mainloop()
+    t.destroy()
 
 __all__ = 'programme_anhalten'.split()
