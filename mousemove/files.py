e9243d78bfb8c34cb5c36151d33a48fe04331880
Nicco Kunzmann <niccokunzmann@googlemail.com>
2013-10-24 20:22:26 +0200

    dateien werden im hintergrund gelöscht-> weniger startup zeit

--- mousemove/files.py
+++ mousemove/files.py
@@ -3,6 +3,7 @@ import os
 import os.path
 import shutil
 import time
+import threading
 
 try: from . import constants
 except SystemError: import constants
@@ -14,23 +15,25 @@ def tempfilename(suffix = '', prefix = ''):
     with _tempfilenamelock:
         return tempfile.mktemp(suffix, 'Stronghold Kingdoms Bot' + os.sep + prefix)
 
-path = os.path.dirname(tempfilename())
-deadline = time.time() - 6 * 60 * 60
-if not os.path.isdir(path):
-    os.mkdir(path)
-files = os.listdir(path)
-for filename in files:
-    filepath = os.path.join(path, filename)
-    try:
-        if os.path.getmtime(filepath) < deadline:
-            if os.path.isfile(filepath):
-                os.remove(filepath)
-            else:
-                shutil.rmtree(filepath)
-    except (FileNotFoundError, PermissionError): pass
+def delete_old_tempfiles():
+    path = os.path.dirname(tempfilename())
+    deadline = time.time() - 6 * 60 * 60 # 6 hours
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    files = os.listdir(path)
+    for filename in files:
+        filepath = os.path.join(path, filename)
+        try:
+            if os.path.getmtime(filepath) < deadline:
+                if os.path.isfile(filepath):
+                    os.remove(filepath)
+                else:
+                    shutil.rmtree(filepath)
+        except (FileNotFoundError, PermissionError): pass
 
-if files: del filename, filepath
-del path, files, deadline
+delete_old_tempfiles_thread = threading.Thread(target = delete_old_tempfiles)
+delete_old_tempfiles_thread.setDaemon(True)
+delete_old_tempfiles_thread.start()
 
 image_folder = os.path.join(os.path.dirname(__file__), 'images')
 if not os.path.isdir(image_folder):
@@ -55,4 +58,5 @@ def screenshot_script_path():
     return os.path.join(os.path.dirname(__file__), '_screenshot script.py')
 
 __all__ = 'tempfilename image_folder forschungsordner config_file_name '\
-          'error_report_file screenshot_script_path'.split()
+          'error_report_file screenshot_script_path delete_old_tempfiles '\
+          'delete_old_tempfiles_thread'.split()
